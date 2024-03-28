9b86ccc0bacab97176d33f064cc0e799e54bc571
nboisclair64 <59835675+nboisclair64@users.noreply.github.com>
2024-03-28 12:43:51 -0400

    Initial Commit

---
+++ FantasyStatTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FantasyStatTracker
+//
+//  Created by NICHOLAS BOISCLAIR on 2024-03-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FantasyStatTracker/FantasyStatTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FantasyStatTrackerApp.swift
+//  FantasyStatTracker
+//
+//  Created by NICHOLAS BOISCLAIR on 2024-03-28.
+//
+
+import SwiftUI
+
+@main
+struct FantasyStatTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
